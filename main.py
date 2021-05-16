633de7c141c9d1b5990d65e170d21efe6e4ce9dd
Jack Grimes <jack.e.t.grimes@gmail.com>
2021-05-16 17:20:52 +0100

    general improvements

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import os
 
 import pandas as pd
 
-from configs import data_path
+from configs import (
+    data_path,
+    loan,
+    payments_divided_equally_over_tax_year_list,
+    paydays_if_allocating_pay_evenly_over_tax_year,
+)
 from utils import (
     calculate_balances_and_interest_added,
     get_and_prep_data,
+    clean_up_overall_df,
+    compare_with_reported_interest,
+    compare_with_reported_balances,
+    convert_floats_to_2_dps,
 )
 
 
@@ -16,35 +25,50 @@ def loan_calculator_runner():
     # 1. getting paid on first day of the month, pay over the tax year being averaged
     # 2. evenly distributed over the months of the tax year
 
-    # todo: payment on last day of month
-
     list_of_dfs = []
 
-    for payments_divided_equally_over_tax_year in [False, True]:
-        for assume_payment_made_day_of_month in [1, 20]:
-            assumptions_string = (
-                ""
-                + payments_divided_equally_over_tax_year
-                * "_payments_divided_equally_over_tax_year"
-                + "_assume_pay_"
-                + str(assume_payment_made_day_of_month)
-            )
+    for (
+        payments_divided_equally_over_tax_year
+    ) in payments_divided_equally_over_tax_year_list:
+        if payments_divided_equally_over_tax_year:
+            for (
+                assume_payment_made_day_of_month
+            ) in paydays_if_allocating_pay_evenly_over_tax_year:
+                list_of_dfs.append(
+                    calculate_balances_and_interest_added(
+                        events,
+                        interest_rates,
+                        payments_divided_equally_over_tax_year,
+                        assume_payment_made_day_of_month,
+                    )
+                )
+        else:
             list_of_dfs.append(
                 calculate_balances_and_interest_added(
                     events,
                     interest_rates,
                     payments_divided_equally_over_tax_year,
-                    assume_payment_made_day_of_month,
-                    assumptions_string,
+                    assume_payment_made_day_of_month=None,
                 )
             )
 
     all_calculations = pd.concat(list_of_dfs, axis=1)
 
-    # todo: add in reported interest added and balances
+    all_calculations = clean_up_overall_df(all_calculations)
+
+    compare_with_reported_interest(all_calculations)
+
+    compare_with_reported_balances(all_calculations)
+
+    all_calculations = convert_floats_to_2_dps(all_calculations)
 
     all_calculations.to_csv(
-        os.path.join(data_path, "calculated_balances_under_various_assumptions.csv",),
+        os.path.join(
+            data_path,
+            loan,
+            "outputs",
+            "calculated_balances_under_various_assumptions.csv",
+        ),
         encoding="utf-8-sig",
         index=False,
     )
--- utils.py
+++ utils.py
@@ -1,38 +1,45 @@
+import calendar
+import datetime
+import logging
 import os
-from datetime import date
 
 import pandas as pd
 
-from configs import data_path
+from configs import data_path, loan
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S"
+)
 
 
 def get_days(year):
-    DATE_END = date(year, 12, 31)
-    DATE_START = date(year - 1, 12, 31)
+    DATE_END = datetime.date(year, 12, 31)
+    DATE_START = datetime.date(year - 1, 12, 31)
     return (DATE_END - DATE_START).days
 
 
 def get_and_prep_data():
     # Read in and clean up data
-
+    logging.info("Reading data")
     events = pd.read_csv(
-        os.path.join(data_path, "loan_events.csv"),
+        os.path.join(data_path, loan, "inputs", "loan_events.csv"),
         dayfirst=True,
         parse_dates=True,
         index_col=["date"],
     )
 
     interest_rates = pd.read_csv(
-        os.path.join(data_path, "interest_rates.csv"),
+        os.path.join(data_path, loan, "inputs", "interest_rates.csv"),
+        dayfirst=True,
         parse_dates=True,
         index_col=["date"],
     )
 
     reported_interest_added = pd.read_csv(
-        os.path.join(data_path, "reported_interest_added.csv"),
+        os.path.join(data_path, loan, "inputs", "reported_interest_added.csv"),
+        dayfirst=True,
         parse_dates=True,
         index_col=["date"],
-        dayfirst=True,
     )
 
     return events, interest_rates, reported_interest_added
@@ -47,13 +54,29 @@ def months_per_tax_year(y):
     return df
 
 
+def convert_floats_to_2_dps(df):
+    for col in df.columns:
+        if df.dtypes[col] == float:
+            df[col] = df[col].apply(lambda x: round(x, ndigits=2))
+    return df
+
+
 def calculate_balances_and_interest_added(
     events,
     interest_rates,
     payments_divided_equally_over_tax_year,
     assume_payment_made_day_of_month,
-    assumptions_string,
 ):
+    assumptions_string = (
+        ""
+        + (
+            payments_divided_equally_over_tax_year
+            * "_payments_divided_equally_over_tax_year"
+        )
+        + ("_assume_pay_" + str(assume_payment_made_day_of_month))
+    )
+
+    logging.info(f"Doing {assumptions_string}")
 
     if payments_divided_equally_over_tax_year:
 
@@ -81,15 +104,28 @@ def calculate_balances_and_interest_added(
 
         repayments_averaged_over_tax_year.set_index(["tax_year"], inplace=True)
 
-        repayments_averaged_over_tax_year["date"] = pd.to_datetime(
-            repayments_averaged_over_tax_year["year_month"]
-            + "-"
-            + str(assume_payment_made_day_of_month),
-            format="%Y-%m-%d",
-        )
-        repayments_averaged_over_tax_year = repayments_averaged_over_tax_year.set_index(
-            ["date"]
-        )
+        if assume_payment_made_day_of_month == "last":
+            days = repayments_averaged_over_tax_year["year_month"].apply(
+                lambda x: calendar.monthrange(
+                    int(x.split("-")[0]), int(x.split("-")[1])
+                )[1]
+            )
+            repayments_averaged_over_tax_year["date"] = pd.to_datetime(
+                repayments_averaged_over_tax_year["year_month"] + "-" + days.astype(str)
+            )
+            repayments_averaged_over_tax_year = repayments_averaged_over_tax_year.set_index(
+                ["date"]
+            )
+        else:
+            repayments_averaged_over_tax_year["date"] = pd.to_datetime(
+                repayments_averaged_over_tax_year["year_month"]
+                + "-"
+                + str(assume_payment_made_day_of_month),
+                format="%Y-%m-%d",
+            )
+            repayments_averaged_over_tax_year = repayments_averaged_over_tax_year.set_index(
+                ["date"]
+            )
 
         events = repayments_averaged_over_tax_year.append([pre_2019_pos, post_2019])
         events.sort_index(inplace=True)
@@ -134,3 +170,81 @@ def calculate_balances_and_interest_added(
 
     all_rows.columns = [col + assumptions_string for col in all_rows.columns]
     return all_rows
+
+
+def clean_up_overall_df(df):
+    columns_only_needed_once = ["date", "annual_interest_rate"]
+    for col in columns_only_needed_once:
+        all_these_cols = [c for c in df.columns if col in c]
+        all_these_cols_to_drop = all_these_cols[1:]
+        df.drop(columns=all_these_cols_to_drop, inplace=True)
+        df.rename(columns={all_these_cols[0]: col}, inplace=True)
+    return df
+
+
+def compare_with_reported_interest(df):
+    df = df.set_index("date")
+    df = df[
+        [col for col in df.columns if (col.startswith("calculated_daily_interest"))]
+    ]
+    df = df.groupby(pd.Grouper(freq="M")).sum()
+    years_and_months = list(zip(df.index.year, df.index.month))
+    years_and_months = [str(x[0]) + "-" + str(x[1]).zfill(2) for x in years_and_months]
+    df.index = pd.Index(years_and_months)
+    for col in df.columns:
+        if "calculated_daily_interest" in col:
+            df.rename(
+                columns={
+                    col: col.replace(
+                        "calculated_daily_interest", "calculated_monthly_interest"
+                    )
+                },
+                inplace=True,
+            )
+
+    reported_interest_added = pd.read_csv(
+        os.path.join(data_path, loan, "inputs", "reported_interest_added.csv"),
+        parse_dates=True,
+    )
+    reported_interest_added.index = reported_interest_added["date"].apply(
+        lambda x: x.split("/")[2] + "-" + x.split("/")[1]
+    )
+
+    df["reported_interest_added_this_month"] = reported_interest_added[
+        "reported_interest_added_this_month"
+    ]
+
+    df = convert_floats_to_2_dps(df)
+
+    df.to_csv(
+        os.path.join(
+            data_path,
+            loan,
+            "outputs",
+            "comparison_calculated_reported_monthly_interest.csv",
+        )
+    )
+
+
+def compare_with_reported_balances(df):
+    df = df.set_index("date")
+    df = df[[col for col in df.columns if (col.startswith("calculated_balance_"))]]
+    reported_balances = pd.read_csv(
+        os.path.join(data_path, loan, "inputs", "reported_balances.csv"),
+        parse_dates=True,
+    )
+
+    reported_balances["date"] = pd.to_datetime(reported_balances["date"])
+    reported_balances.set_index(["date"], drop=True, inplace=True)
+
+    df["reported_balance"] = reported_balances["reported_balance"]
+
+    df.dropna(subset=["reported_balance"], inplace=True)
+
+    df = convert_floats_to_2_dps(df)
+
+    df.to_csv(
+        os.path.join(
+            data_path, loan, "outputs", "comparison_calculated_reported_balances.csv"
+        ),
+    )
